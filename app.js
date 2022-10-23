5b8bd2b7117f41a47df1d5c4300a6b2cc7a9132e
Oleksii <oleksii.y@peeklogic.com>
2022-10-23 12:25:52 +0300

    speedify

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const async = require('async');
 const path = require('path');
 const publicPath = path.join(__dirname, './');
 const port = process.env.PORT || 7000;
 const app = express();
 
-app.use(express.static(publicPath))
+function parallel(middlewares){
+    return function (req, res, next){
+        async.each(middlewares, (mw, cb) => {
+            mw(req, res, cb);
+        }, next)
+    }
+}
+
+app.use(express.static(publicPath));
+app.use(parallel(express.middlewares));
 
 app.get('/', (req, res) => {
     res.status(200).send(publicPath);
