7cc099a5ef35b7769c0534e4cc35962bf771f879
Oleksii <oleksii.y@peeklogic.com>
2022-10-23 18:41:39 +0300

    remove

--- index.js
+++
@@ -1,7 +0,0 @@
-const root = document.getElementById('root');
-
-for(let i = 0; i < 10; i++){
-    let element = document.createElement('p');
-    element.innerHTML = 'Abstract #' + i.toString();
-    root.appendChild(element);
-}
