53c53f28aad8c080ce2d97a650bf675d071f7828
Oleksii <oleksii.y@peeklogic.com>
2022-10-24 15:41:49 +0300

    add login page

---
+++ main.js
@@ -0,0 +1,4 @@
+const root = document.getElementById('root');
+const image = document.createElement('img');
+image.src = 'https://i.pinimg.com/originals/82/40/8e/82408e71f82f1017903e2165f63c4742.jpg';
+root.appendChild(image);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import * as reactDom from 'react-dom/client';
 
-class Button extends React.Component {
+class GreetingForm extends React.Component {
     render(){
-        return (<button>Btn</button>);
+        return (<span class="inline_elements">
+            <p onClick={logIn} class="inline_text">Sign Up</p>
+            <p onClick={signUp} class="inline_text">Log In</p>
+        </span>);
     }
 }
-reactDom.createRoot(document.getElementById('root')).render(<Button />);
+
+class LogInForm extends React.Component {
+    render(){
+        return (<div id="cred" hidden={true}>
+            <div>
+                <input type="text" class="cred_inp" placeholder='username'></input>
+            </div>
+            <div>
+                <input type="password" class="cred_inp" placeholder='password'></input>
+            </div>
+            <div id="submit_button_div">
+                <input type="submit" 
+                class="cred_inp" 
+                value="Submit" 
+                id="submit_button" onClick={goToMain}></input>
+            </div>
+        </div>);
+    }
+}
+
+class GreetTitle extends React.Component {
+    render(){
+        return (<div id="gt" hidden={false}>
+            <p>The Lambda</p>
+            <div>new era of communication</div>
+        </div>);
+    }
+}
+
+class App extends React.Component {
+    render(){
+        return (<div>
+            <GreetingForm />
+            <GreetTitle />
+            <LogInForm />
+        </div>)
+    }
+}
+
+function logIn(){
+    document.getElementById('cred').hidden = false;
+    document.getElementById('gt').hidden = true;
+}
+
+function signUp(){
+    document.getElementById('cred').hidden = false;
+    document.getElementById('gt').hidden = true;
+}
+
+function goToMain(){
+    let currentUrl = document.location.href;
+    let followUrl = currentUrl + 'main.html';
+    window.open(followUrl, '_self');
+}
+
+reactDom.createRoot(document.getElementById('root')).render(<App />);
